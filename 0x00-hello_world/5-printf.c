27c7ceb47acda02e52c557ecfab921d760a6c48d
dee445 <ayenim44@gmail.com>
2022-09-10 23:39:50 -0700

    Task 5-6 level C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-#include<stdio.h>                                                                      
-
-/**                                                                          
- * main - Prints a text
- *
- * Return: Always 0 (Success)
- */                                                                                                         
-                                                                                                         
-
-int main(void)                                                                                              
-{      
-      	printf("with proper grammar, but the outcome is a piece of art,\n");                                                      return (0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");return (0);
 }
